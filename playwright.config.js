518203fb44b656b5fe362c8ff0dc877a39e17350
unknown <ggmanikantaee026@gmail.com>
2026-04-24 12:50:55 +0530

    changed

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
+  testDir: './tests',
   use: {
     headless: true, // CI will use this
     baseURL: 'http://localhost:3000',
--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-// Simple Jest test to ensure CI/CD passes
-
-describe('Basic Test Suite', () => {
-  test('should always pass', () => {
-    expect(true).toBe(true);
-  });
-});
